433bcc082beec9a7cf80e73b48ffeffa38e20062
Alan Walker X <90819785+XAlanWalkerX@users.noreply.github.com>
2024-07-30 18:54:45 +0530

    Update downloader.js

--- plugins/downloader.js
+++ plugins/downloader.js
@@ -1,6 +1,7 @@
 
  
-const { tlang, ringtone, cmd, fetchJson, sleep, botpic, ffmpeg, getBuffer, pinterest, prefix, Config } = require('../lib');
+const { tlang, ringtone, fetchJson, sleep, botpic, ffmpeg, getBuffer, pinterest, prefix, Config } = require('../lib');
+const cmd = require('../lib/plugins.js');
 const { mediafire } = require("../lib/mediafire.js");
 const googleTTS = require("google-tts-api");
 const ytdl = require('ytdl-secktor')
